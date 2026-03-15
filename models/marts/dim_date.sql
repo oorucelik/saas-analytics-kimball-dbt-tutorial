9a862e78c416fa4095193498dda1faa959a27913
oorucelik <o.oruccelik@gmail.com>
2026-03-15 23:49:30 +0300

    feat: Add 'dim_date' model with date_spine instead dbt_date.

--- models/marts/dim_date.sql
+++ models/marts/dim_date.sql
@@ -1,18 +1,19 @@
-with date_dimension as (
-    {{ dbt_date.get_date_dimension("2023-01-01", "2025-01-01") }}
-),
-final as (
-    select
-        {{ dbt_utils.generate_surrogate_key(['date_day']) }} as date_key,
-        date_day,
-        year_number as year,
-        month_of_year as month,
-        quarter_of_year as quarter,
-        day_of_week,
-        month_name,
-        -- dbt-date provides 'is_weekend' out of the box (boolean)
-        is_weekend
-    from date_dimension
-)
-
-select * from final
+SELECT
+    CAST(range AS DATE) AS date_day,
+    year(range) AS date_year,
+    month(range) AS date_month,
+    monthname(range) AS date_monthname,
+    day(range) AS date_dayofmonth,
+    dayofweek(range) AS date_dayofweek,
+    week(range) AS date_weekofyear,
+    dayofyear(range) AS date_dayofyear,
+    CAST(CASE WHEN dayofweek(range) IN (0, 6) THEN 1 ELSE 0 END AS BOOLEAN) AS date_is_weekend
+FROM 
+    (
+        {{ dbt_utils.date_spine(
+            datepart="day",
+            start_date="2023-01-01",
+            end_date="2025-01-01"
+            )
+        }}
+    ) AS date_series
