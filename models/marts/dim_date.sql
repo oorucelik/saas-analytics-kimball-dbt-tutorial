d31f7789059cef15ba94ba07485fab3df9260e8f
oorucelik <o.oruccelik@gmail.com>
2026-03-17 16:43:34 +0300

    fix: Tests and dim_date corrected.

--- models/marts/dim_date.sql
+++ models/marts/dim_date.sql
@@ -1,19 +1,18 @@
 SELECT
-    CAST(range AS DATE) AS date_day,
-    year(range) AS date_year,
-    month(range) AS date_month,
-    monthname(range) AS date_monthname,
-    day(range) AS date_dayofmonth,
-    dayofweek(range) AS date_dayofweek,
-    week(range) AS date_weekofyear,
-    dayofyear(range) AS date_dayofyear,
-    CAST(CASE WHEN dayofweek(range) IN (0, 6) THEN 1 ELSE 0 END AS BOOLEAN) AS date_is_weekend
+    CAST(strftime(date_day, '%Y%m%d') AS INT) AS date_key,
+    CAST(date_day AS DATE) AS date_day,
+    year(date_day) AS date_year,
+    month(date_day) AS date_month,
+    monthname(date_day) AS date_monthname,
+    day(date_day) AS date_dayofmonth,
+    dayofweek(date_day) AS date_dayofweek,
+    week(date_day) AS date_weekofyear,
+    dayofyear(date_day) AS date_dayofyear,
+    CAST(CASE WHEN dayofweek(date_day) IN (0, 6) THEN 1 ELSE 0 END AS BOOLEAN) AS date_is_weekend
 FROM 
-    (
-        {{ dbt_utils.date_spine(
+    ({{ dbt_utils.date_spine(
             datepart="day",
-            start_date="2023-01-01",
-            end_date="2025-01-01"
-            )
-        }}
+            start_date="CAST('2023-01-01' AS TIMESTAMP)",
+            end_date="CAST('2025-01-01' AS TIMESTAMP)",
+            )}}
     ) AS date_series
